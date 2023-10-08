b9f872a9562667df92a0c39dd3643cc56b5759cb
hacker-flavio <theflavioherrera@gmail.com>
2023-10-08 15:21:47 -0700

    added frontend and backend edits

---
+++ flask-react/trash/OldHome.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+// import axiosInstance from "../config/axiosConfig";
+//import axios from "axios";
+
+import axios from "../config/axiosConfig"; // Import your custom Axios instance
+
+function Home() {
+  const [profileData, setProfileData] = useState(null);
+
+  function getData() {
+    axios({
+      method: "GET",
+      url: "/profile",
+    })
+      .then((response) => {
+        const res = response.data;
+        setProfileData({
+          profile_name: res.name,
+          about_me: res.about,
+        });
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        }
+      });
+  }
+
+  return (
+    <div className="App">
+      <p>To get your profile details: </p>
+      <button onClick={getData}>Click me</button>
+      {profileData && (
+        <div>
+          <p>Profile name: {profileData.profile_name}</p>
+          <p>About me: {profileData.about_me}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Home;
